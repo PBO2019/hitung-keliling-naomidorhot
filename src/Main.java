020e322ba351c51b2131bd3536d7bb6a2f09609f
Naomi Dorhot Septiana Silitonga <naomidorhot08@gmail.com>
2019-01-28 14:51:17 +0700

    Ini program yang baru

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,16 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Lingkaran lingkaran = new Lingkaran();
+        bujurSangkar bujursangkar = new bujurSangkar();
+
+        bujursangkar.setSisi(7);
+        bujursangkar.hitungKeliling();
+        System.out.println("Keliling Bujur Sangkar = " + bujursangkar.getHasil());
+
+        lingkaran.setRadius(7);
+        lingkaran.setphi(3.14);
+        lingkaran.hitungKeliling();
+        System.out.println("Keliling Lingkran = " + lingkaran.getkeliling());
     }
 }
